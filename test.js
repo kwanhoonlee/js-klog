1400c5c7c5539bcd40a0883223a352b1e3bd069c
kwanhoonlee <rok.lee91@gmail.com>
2019-04-17 01:42:02 +0900

    modified node-dependency

--- test.js
+++ test.js
@@ -1,4 +1,21 @@
 const IPFS = require('ipfs')
-
 const OrbitDB = require('orbit-db')
+const ipfsOptions = {
+  EXPERIMENTAL: {
+    pubsub:true
+  },
+}
+const ipfs = new IPFS(ipfsOptions)
+ipfs.on('ready', async () => {
+  const orbitdb = await OrbitDB.createInstance(ipfs)
+  const options = {
+    // Give write access to ourselves
+    accessController: {
+      write: [orbitdb.identity.publicKey]
+    }
+  }
 
+  const db = await orbitdb.keyvalue('first-database', options)
+  console.log(db.address.toString())
+  // /orbitdb/Qmd8TmZrWASypEp4Er9tgWP4kCNQnW4ncSnvjvyHQ3EVSU/first-database
+})
